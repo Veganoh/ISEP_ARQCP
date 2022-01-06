f04c686e5cafbc540e824a9f4b4092eb6d52e7c1
Miguel Silva <mafs687@gmail.com>
2022-01-06 19:41:19 +0000

    Added exercise 17a

---
+++ modulo4/ex17a/greater_date.c
@@ -0,0 +1,27 @@
+unsigned int greater_date(unsigned int date1, unsigned int date2){
+
+
+	short year1 = (short)(date1 >> 8);			# Coloca o ano1 nos primeiros dois bytes 
+	short year2 = (short)(date2 >> 8);			# Coloca o ano2 nos primeiros dois bytes
+
+	if (year1 > year2) return date1;
+	
+	if (year2 > year1) return date2;
+	
+	char month1 = (char)(date1 >> 24);			# Coloca o mês1 no primeiro byte
+	char month2 = (char)(date2 >> 24);			# Coloca o mês2 no segundo byte
+	
+	if (month1 > month2) return date1;
+	
+	if (month2 > month1) return date2;
+	
+	
+	char day1 = (char)date1;				   # Coloca o dia1 no primeiro byte
+	char day2 = (char)date2;				   # Coloca o dia2 no primeiro byte
+	
+	if (day1 > day2) return date1;
+	
+	if (day2 > day1) return date2;
+	
+	return date1;
+}
---
+++ modulo4/ex17a/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "greater_date.h"
+
+
+unsigned int build_date( int year , int month , int day ) {
+
+	return  (year << 8)  | (month <<24 ) |  day ;
+}
+
+int main(){
+	
+	unsigned int  result = greater_date(build_date(2017,11,11),build_date(12,12,31));
+	
+	printf("Result: %u \n",result);
+	
+	
+	
+	return 0;
+}
